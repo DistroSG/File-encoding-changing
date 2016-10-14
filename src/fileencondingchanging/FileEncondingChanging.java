718729e8864e012783d0513e153949deff68ddac
distro <distro@Diskone.lan>
2016-10-14 14:27:31 +0300

    Adding new libs and new methods

--- src/fileencondingchanging/FileEncondingChanging.java
+++ src/fileencondingchanging/FileEncondingChanging.java
@@ -5,8 +5,16 @@
  */
 package fileencondingchanging;
 
+import com.ibm.icu.text.CharsetDetector;
+import com.ibm.icu.text.CharsetMatch;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.UnsupportedCharsetException;
+import org.apache.commons.io.FileUtils;
 
 /**
  *
@@ -17,39 +25,70 @@ public class FileEncondingChanging {
     private final String filename;
     private final File file;
     private String isReadOnly;
+    private Reader reader;
+    private String charset;
 
     public FileEncondingChanging(String filename) {
         this.filename = filename;
         file = new File(filename);
     }
 
-    private void changeTheReadOnlyAttribute() {
+    private void turOnReadOnlyAttribute() {
+        if (file.canWrite() == true) {
+            file.setWritable(false);
+        }
+    }
+
+    private void turOffReadOnlyAttribute() {
         if (file.canWrite() == false) {
             file.setWritable(true);
-        } else {
-            file.setWritable(false);
         }
     }
 
     private String isReadOnly() {
 
         if (file.canWrite() == true) {
-            isReadOnly = "Read Only is on";
-        } else {
             isReadOnly = "Read Only is off";
+        } else {
+            isReadOnly = "Read Only is on";
         }
         return isReadOnly;
     }
 
+    private String getEncode() throws FileNotFoundException, IOException {
+        FileInputStream is = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(is);
+        CharsetDetector cd = new CharsetDetector();
+        cd.setText(bis);
+        CharsetMatch cm = cd.detect();
+
+        if (cm != null) {
+            charset = cm.getName();
+        } else {
+            throw new UnsupportedCharsetException(filename);
+        }
+        return charset;
+    }
+
+    private void changeToUTF8() throws IOException {
+        String content = FileUtils.readFileToString(file, charset)  + '\ufeff';
+        FileUtils.write(file, content, "UTF-8");
+
+    }
+
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        FileEncondingChanging test = new FileEncondingChanging("C:\\test\\Пример"
-                + " (2016).cue");
+    public static void main(String[] args) throws IOException {
+        FileEncondingChanging test = new FileEncondingChanging("C:\\test\\ДДТ - Свинья на радуге, 1982.cue");
         System.out.println(test.isReadOnly());
-        test.changeTheReadOnlyAttribute();
+        test.turOffReadOnlyAttribute();
         System.out.println(test.isReadOnly());
+        System.out.println(test.getEncode());
+        test.changeToUTF8();
+        System.out.println(test.getEncode());
+        test.turOnReadOnlyAttribute();
 
     }
 
